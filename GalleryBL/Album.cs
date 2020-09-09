b4eba8f2f279771acfb6e9b96d2f899f42a878bd
Tarek Alzein <tfzein@gmail.com>
2020-09-09 10:57:36 +0200

    First Commit. Created basic project and class libraries to start with.

---
+++ GalleryBL/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalleryBL
+{
+    public class Album
+    {
+        public string AlbumTitle { get; set; }
+        public List<MediaFile> AlbumFiles { get; set; }
+        public string AlbumDescription { get; set; }
+    }
+}
---
+++ GalleryBL/MediaFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalleryBL
+{
+    /// <summary>
+    /// Class to represent a media file and its attributes.
+    /// </summary>
+    public class MediaFile
+    {
+        public string FileName { get; set; }
+        public string Description { get; set; }
+        public string FilePath { get; set; }
+    }
+}
