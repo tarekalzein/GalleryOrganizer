facb53c9e5058c17d3899009481099e8cb78a1e5
Tarek Alzein <tfzein@gmail.com>
2020-09-22 16:10:46 +0200

    SlideShowWindow opens in fullscreen now: change windowstate to maximized and windowstyle to none. added esc key listener to close window when esc button is pressed.

--- GalleryPL/SlideShowWindow.xaml.cs
+++ GalleryPL/SlideShowWindow.xaml.cs
@@ -1,21 +1,7 @@
 ﻿using GalleryBL;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace GalleryPL
 {
@@ -38,6 +24,7 @@ namespace GalleryPL
         public SlideShowWindow(Album a)
         {                        
             InitializeComponent();
+            this.PreviewKeyDown += new KeyEventHandler(EscButtonHandler); //listener to ESC button to skip the slideshow and close the window
 
             album = a;
             if (album.MediaFiles.Count >0)
@@ -93,6 +80,11 @@ namespace GalleryPL
             if (currentIndex + 1 != album.MediaFiles.Count)
                 currentIndex++;
             ShowMediaFileAtIndex(currentIndex);
+        }
+        private void EscButtonHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Close();
         }
     }
 }
